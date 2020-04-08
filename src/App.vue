01738f38080e7bda6279933487046ee1f108a1d7
Tomasz Wiśniewski <tomasz.a.wisniewski@comarch.com>
2020-04-08 14:11:05 +0200

    +dash bar items

--- src/App.vue
+++ src/App.vue
@@ -53,8 +53,13 @@ export default {
     return {
       drawer: true,
       items: [
-        { title: "Dashboard", icon: "mdi-view-dashboard", to: "/dashboard" },
-        { title: "About", icon: "mdi-help-box" }
+        { title: "Strona główna", icon: "$vuetify.icons.dash", to: "/dash" },
+        { title: "Najlepsze", icon: "$vuetify.icons.star", to: "/top" },
+        { title: "Artyści", icon: "$vuetify.icons.artist", to: "/artists" },
+        { title: "Piosenki", icon: "$vuetify.icons.music", to: "/songs" },
+        { title: "Słuchaj", icon: "$vuetify.icons.play", to: "/playMusic" },
+        { title: "Ustawienia", icon: "$vuetify.icons.prefs", to: "/settings" },
+        { title: "Wyloguj się", icon: "$vuetify.icons.logout", to: "/logout" }
       ],
       color: "primary",
       colors: ["primary", "blue", "success", "red", "teal"],
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,20 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
 
-const opts = {}
-
-export default new Vuetify(opts)
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+    values: {
+      dash: 'mdi-view-dashboard',
+      star: 'mdi-star',
+      artist: 'mdi-artist',
+      music: 'mdi-music',
+      play: 'mdi-play',
+      prefs: 'mdi-settings',
+      logout: 'mdi-location-exit'
+    },
+  },
+});
