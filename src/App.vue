b7c82119e199b687cef96afb2f09c064315484b2
Tomasz Wiśniewski <tomasz.a.wisniewski@comarch.com>
2020-04-08 13:07:23 +0200

    +permanent dashboard in main view

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,98 @@
 <template>
-    <div id="app">
-        <div id="nav">
-            <router-link to="/home">Home</router-link>|
-            <router-link to="/dashboard">Dashboard</router-link>
-            <router-link to="/about">About</router-link>
+  <v-app>
+    <v-navigation-drawer
+      app
+      permanent
+      :color="color"
+      :expand-on-hover="expandOnHover"
+      :mini-variant="Variantmini"
+      :src="bg"
+      absolute
+      dark
+    >
+      <v-list dense nav class="py-0">
+        <v-list-item two-line :class="miniVariant && 'px-0'">
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/men/81.jpg" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Zalogowano</v-list-item-title>
+            <v-list-item-subtitle>Username</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-content>
+      <v-container fluid>
+        <div id="app">
+          <div id="nav"></div>
         </div>
-        <router-view />
-    </div>
+      </v-container>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: true,
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard", to: "/dashboard" },
+        { title: "About", icon: "mdi-help-box" }
+      ],
+      color: "primary",
+      colors: ["primary", "blue", "success", "red", "teal"],
+      right: false,
+      miniVariant: true,
+      expandOnHover: true,
+      background: true
+    };
+  },
+  computed: {
+    bg() {
+      return this.background
+        ? "https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+        : undefined;
+    }
+  }
+};
+</script>
 
 <style>
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
 #nav {
-    padding: 30px;
+  padding: 30px;
 }
 
 #nav a {
-    font-weight: bold;
-    color: #2c3e50;
+  font-weight: bold;
+  color: #2c3e50;
 }
 
 #nav a.router-link-exact-active {
-    color: #42b983;
+  color: #42b983;
 }
 </style>
